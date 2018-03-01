2106cc68739b081023121ef3f087d81da8f96373
Urie Choi <urie@Uries-MacBook-Pro.local>
2018-03-01 18:08:43 -0500

    about page, logo components

--- App.js
+++ App.js
@@ -1,50 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Font } from 'expo';
+import { About } from './About.js';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{flexDirection: 'row', paddingTop: 30}}>
-          <View>
-            <View style={styles.pongRoundedRectangle}>
-              <Text style={styles.pongMatesText}>Pong</Text>
-            </View>
-          </View>
-          <View style={{paddingTop: 30}}>
-            <View style={styles.matesRoundedRectangle}>
-              <Text style={styles.pongMatesText}>Mates</Text>
-            </View>
-          </View>
-        </View>
-      </View>
-    );
+      <About>
+      </About>
+    )
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  pongRoundedRectangle: {
-    backgroundColor: 'orange',
-    width: 220,
-    height: 120,
-    borderTopRightRadius: 30,
-    borderBottomRightRadius: 30,
-  },
-  matesRoundedRectangle: {
-    backgroundColor: 'yellow',
-    width: 240,
-    height: 120,
-    borderTopLeftRadius: 30,
-    borderBottomLeftRadius: 30,
-  },
-  pongMatesText: {
-    paddingTop: 20,
-    padding: 5,
-    fontFamily: 'Cochin',
-    fontSize: 50,
-    alignItems: 'center'
-  },
-});
+};
