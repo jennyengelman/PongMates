831b9c0a0f21e115f52d7583f18601ab7bb7579b
Alexandra Milich <alexandra.tmilich@gmail.com>
2018-03-24 22:53:03 -0400

    almost done, still having small error with imported font

--- App.js
+++ App.js
@@ -3,6 +3,15 @@ import { StyleSheet, Text, View, Image, Fonts, Button } from 'react-native';
 import { Font } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'double-bubble-shadow': require('./assets/fonts/Double_Bubble_shadow.otf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -13,17 +22,28 @@ export default class App extends React.Component {
           <View style = {styles.topButtons}>
             <Text style = {styles.faqButton}>?</Text>
           </View>
-
         </View>
+
         <View style = {{justifyContent: 'center'}}>
 
-          <View style = {styles.postButton}>
-            <Text style = {styles.buttonText}>Post a Game</Text>
+          <View style = {{width: '100%', flexDirection: 'row', justifyContent: 'flex-start'}}>
+            <Logo font = {this.state.fontLoaded}> </Logo>
+            <View style = {styles.postButton}>
+            {
+              this.state.fontLoaded ? (
+                <Text style = {styles.postFontStyle}>Create a Game</Text>
+
+              ) : null
+            }
+            </View>
+          </View>
+          <View style = {{width: '100%', flexDirection: 'row', justifyContent: 'flex-end'}}>
+            <View style = {styles.findButton}>
+              <Text style = {styles.findFontStyle}>Find a</Text>
+              <Text style = {styles.findFontStyle}>Game</Text>
+            </View>
           </View>
 
-          <View style = {styles.findButton}>
-            <Text style = {styles.buttonText}>Find a Game</Text>
-          </View>
         </View>
       </View>
     );
@@ -31,6 +51,18 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  postFontStyle: {
+    color: 'white',
+    fontSize: 45,
+    textAlign: 'left',
+    marginLeft: 15,
+  },
+  findFontStyle:{
+    color: 'white',
+    fontSize: 45,
+    textAlign: 'left',
+    marginLeft: 15,
+  },
   container: {
     flexDirection: 'column',
     flex: 1,
@@ -38,18 +70,16 @@ const styles = StyleSheet.create({
   },
   faqButton: {
     fontSize: 70,
+    color: '#C2515B',
   },
   topButtons: {
-    width: 130,
+    width: 120,
     height: 120,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     backgroundColor:'#F5F5F5',
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  buttonText:{
-    fontSize: 40,
   },
   image:{
     width: 70,
@@ -58,21 +88,27 @@ const styles = StyleSheet.create({
   postButton: {
     marginBottom: 30,
     marginTop: 20,
-    width: 300,
+    width: '80%',
     height: 250,
-    borderRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomRightRadius: 20,
     borderColor: 'white',
     borderWidth: 5,
     paddingTop: 50,
     backgroundColor:'#FFC928',
+    shadowColor: 'gray',
+    shadowOpacity: 0.5,
   },
   findButton:{
     width: 300,
     height: 250,
     backgroundColor:'#93E1FA',
-    borderRadius: 20,
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
     borderColor: 'white',
     borderWidth: 5,
     paddingTop: 50,
+    shadowColor: 'gray',
+    shadowOpacity: .5,
   }
 });
