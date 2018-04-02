5e55c1273da6de1c0ae81f5bf957a0a04e493378
Alexandra Milich <alexandra.tmilich@gmail.com>
2018-04-02 14:20:57 -0400

    fixes most of the issues on the PR

--- App.js
+++ App.js
@@ -10,39 +10,44 @@ export default class App extends React.Component {
     await Font.loadAsync({
       'double-bubble-shadow': require('./assets/fonts/Double_Bubble_shadow.otf'),
     });
-    this.setState({ fontLoaded: true });
+    this.setState({ fontLoaded: true }) ;
   }
   render() {
     return (
       <View style={styles.container}>
-        <View style = {{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <View style = {styles.topButtons}>
-            <Image source = {require('./assets/profile.png')} style = {styles.image}/>
-          </View>
-          <View style = {styles.topButtons}>
-            <Text style = {styles.faqButton}>?</Text>
-          </View>
-        </View>
+          <View style = {{ height: '10%', flexDirection: 'row', justifyContent: 'space-around', alignContent: 'flex-start'}}>
+            <View style = {styles.topButtons}>
+              <Image source = {require('./assets/profile.png')} style = {styles.image}/>
+            </View>
+            <View style = {styles.topButtons}>
+            {
+              this.state.fontLoaded ? (
+                <Text style = {styles.faqButton}>?</Text>
+              ) : null
+            }
 
-        <View style = {{justifyContent: 'center'}}>
+            </View>
+          </View>
 
-          <View style = {{width: '100%', flexDirection: 'row', justifyContent: 'flex-start'}}>
-            
+        <View style = {{ height: '90%', justifyContent: 'center', alignItems: 'center'}}>
+          <View style = {{ width: '100%', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
             <View style = {styles.postButton}>
             {
               this.state.fontLoaded ? (
-                <Text style = {styles.postFontStyle}>Create a Game</Text>
+                <Text style = {styles.postFontStyle}>CREATE A GAME</Text>
               ) : null
             }
             </View>
           </View>
-          <View style = {{width: '100%', flexDirection: 'row', justifyContent: 'flex-end'}}>
+          <View style = {{ width: '100%', flexDirection: 'row', justifyContent: 'flex-end'}}>
             <View style = {styles.findButton}>
-              <Text style = {styles.findFontStyle}>Find a</Text>
-              <Text style = {styles.findFontStyle}>Game</Text>
+            {
+                this.state.fontLoaded ? (
+                  <Text style = {styles.findFontStyle}>FIND A GAME</Text>
+                ) : null
+            }
             </View>
           </View>
-
         </View>
       </View>
     );
@@ -52,28 +57,34 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   postFontStyle: {
     color: 'white',
-    fontSize: 45,
-    textAlign: 'left',
-    marginLeft: 15,
+    fontSize: 60,
+    textAlign: 'right',
+    marginLeft: 10,
+    paddingRight: 25,
+    fontFamily: 'double-bubble-shadow'
   },
   findFontStyle:{
     color: 'white',
-    fontSize: 45,
+    fontSize: 60,
     textAlign: 'left',
     marginLeft: 15,
+    paddingLeft: 10,
+    fontFamily: 'double-bubble-shadow'
   },
   container: {
+    justifyContent: 'center',
     flexDirection: 'column',
     flex: 1,
     backgroundColor: '#F2994A',
   },
   faqButton: {
-    fontSize: 70,
+    fontSize: 80,
     color: '#C2515B',
+    fontFamily: 'double-bubble-shadow'
   },
   topButtons: {
     width: 120,
-    height: 120,
+    height: 130,
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
     backgroundColor:'#F5F5F5',
@@ -92,11 +103,14 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     borderBottomRightRadius: 20,
     borderColor: 'white',
-    borderWidth: 5,
+    borderTopWidth: 5,
+    borderRightWidth: 5,
+    borderBottomWidth: 5,
     paddingTop: 50,
     backgroundColor:'#FFC928',
     shadowColor: 'gray',
-    shadowOpacity: 0.5,
+    shadowOpacity: .8,
+    shadowOffset: {width: 0, height: 5}
   },
   findButton:{
     width: 300,
@@ -105,10 +119,13 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 20,
     borderBottomLeftRadius: 20,
     borderColor: 'white',
-    borderWidth: 5,
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    borderLeftWidth: 5,
     paddingTop: 50,
+    marginTop: 22,
     shadowColor: 'gray',
-    shadowOpacity: .5,
-    /*fontFamily: 'double-bubble-shadow'*/
+    shadowOpacity: .8,
+    shadowOffset: {width: 6, height: 6}
   }
 });
