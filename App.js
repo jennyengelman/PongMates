7c5b8d078895e66a223412d132dd71ed6864f23a
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2018-04-02 18:22:19 -0400

    fixed PR

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, Image, TextInput, StackNavigator, Font } from 'react-native';
-import { HomeScreen } from './Home.js';
-
-// const App = StackNavigator({
-//   Home: { screen: HomeScreen },
-//   Selection: { screen: SelectionScreen },
-// });
-// <View style={styles.removeHeaderOverlap}>
-// </View>
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <HomeScreen>
-      </HomeScreen>
-    )
+      <View style={styles.container}>
+        <View style={{flexDirection: 'row', paddingTop: 30}}>
+          <View>
+            <View style={styles.pongRoundedRectangle}>
+              <Text style={styles.pongMatesText}>Pong</Text>
+            </View>
+          </View>
+          <View style={{paddingTop: 30}}>
+            <View style={styles.matesRoundedRectangle}>
+              <Text style={styles.pongMatesText}>Mates</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
   }
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pongRoundedRectangle: {
+    backgroundColor: 'orange',
+    width: 220,
+    height: 120,
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  matesRoundedRectangle: {
+    backgroundColor: 'yellow',
+    width: 240,
+    height: 120,
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+  },
+  pongMatesText: {
+    paddingTop: 20,
+    padding: 5,
+    fontFamily: 'Cochin',
+    fontSize: 50,
+    alignItems: 'center'
+  },
+});
