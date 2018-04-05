fcbbed37dd59d0d75635c1f4f4d35af47d9471b0
Alexandra Milich <alexandra.tmilich@gmail.com>
2018-04-05 10:39:51 -0400

    added fonts, still working on images

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ const RootStack = StackNavigator({
 export default class App extends React.Component {
   render() {
     return (
-      <RootStack />
+      <RootStack/>
     )
   }
 };
--- MeetMatch.js
+++ MeetMatch.js
@@ -3,26 +3,40 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { Font } from 'expo';
 
 export default class App extends React.Component {
-
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'double-bubble': require('./assets/fonts/Double_Bubble_shadow.otf'),
+      'bubble-body': require('./assets/fonts/Bubbleboddy-FatTrial.ttf'),
+      'source-sans': require('./assets/fonts/source-sans-pro.semibold.ttf')
+    });
+    this.setState({ fontLoaded: true }) ;
+  }
   render(){
     return(
       <View style = { styles.container }>
         <View style = { styles.topContainer }>
-          <Text style = { styles.matchText }>
-            MATCH
-          </Text>
+          <View>
+            <Text style = { this.state.fontLoaded ? styles.matchText : styles.anything }>
+              IT'S A MATCH!'
+            </Text>
+          </View>
+          <Image source = { require ('./assets/ball.png')} style = { styles.ballStyle }/>
+          <Image source = { require ('./assets/person.png')} style = { styles.personStyle }/>
         </View>
 
         <View style = { styles.bottomContainer }>
           <View style = { styles.matchDetails }>
-            <Text style = { styles.detailsStyle}>
-              Name: {'\n\n'}Place: {'\n\n'}Time:
+            <Text style = { this.state.fontLoaded ? styles.detailsStyle : styles.anything }>
+              Name: {'\n'}Place: {'\n'}Time:
             </Text>
           </View>
         </View>
 
         <View style = { styles.continue }>
-          <Text style = { styles.continueText }>
+          <Text style = { this.state.fontLoaded ? styles.continueText : styles.anything }>
             tap to continue
           </Text>
         </View>
@@ -40,12 +54,27 @@ const styles = StyleSheet.create({
   topContainer: {
     flex: .6,
     backgroundColor:'#fff',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   matchText: {
       textAlign: 'center',
       fontWeight: 'bold',
-      fontSize: 30
+      fontSize: 50,
+      fontFamily: 'double-bubble'
+  },
+  imageContainer:{
+    width: 500,
+    height:500
+  },
+  ballStyle: {
+    width: '50%',
+    height: '50%',
+    padding: -50,
+  },
+  personStyle: {
+    width: '20%',
+    height: '20%',
   },
   bottomContainer: {
     flex: .33,
@@ -64,9 +93,10 @@ const styles = StyleSheet.create({
   },
   detailsStyle: {
     fontWeight: 'bold',
-    marginLeft: 12,
-    fontSize: 20,
+    margin: 15,
+    fontSize: 28,
     color: '#4F4F4F',
+    fontFamily: 'source-sans'
   },
   continue: {
     flex: .07,
@@ -76,7 +106,8 @@ const styles = StyleSheet.create({
   continueText: {
     textAlign: 'center',
     fontWeight: 'bold',
-    fontSize: 23,
-    color: '#fff'
+    fontSize: 25,
+    color: '#fff',
+    fontFamily: 'source-sans'
   }
 });
