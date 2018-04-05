786ab12eb511119ce0f36fb88b8fdf90f07b52d2
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2018-04-05 18:11:01 -0400

    completed basic layout of page

--- screens/FoundGamePage.js
+++ screens/FoundGamePage.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, Alert, Image, TextInput } from 'react-n
 import { Font } from 'expo';
 import { StackNavigator } from 'react-navigation';
 
-export class HomeScreen extends React.Component {
+export class FoundGameScreen extends React.Component {
   state = {
     fontLoaded: false,
    };
@@ -18,44 +18,131 @@ export class HomeScreen extends React.Component {
     return (
       <View style = { styles.container }>
         <View style = { styles.topContainer }>
-          <Text style = { this.state.fontLoaded ? styles.match : styles.matchElse }>Its a Match!</Text>
+          <Text style = { this.state.fontLoaded ? styles.matchText : styles.matchTextElse }>Its a Match!</Text>
           <Image style = { styles.cup } source = {require('./../assets/cup.png')}/>
         </View>
+        <View style = { styles.yellowStrip }>
+        </View>
         <View style = { styles.bottomContainer }>
-          <View style = { styles.yellowStrip }>
+          <View style = { styles.detailsHeader }>
+            <Text style = { this.state.fontLoaded ? styles.detailsHeaderText : styles.detailsHeaderTextElse }>Your Game Details</Text>
           </View>
-
+          <View style = { styles.details }>
+            <Text style = { this.state.fontLoaded ? styles.detailsText : styles.detailsTextElse }>Partner:</Text>
+            <Text style = { this.state.fontLoaded ? styles.detailsText : styles.detailsTextElse }>Place:</Text>
+            <Text style = { this.state.fontLoaded ? styles.detailsText : styles.detailsTextElse }>Time:</Text>
+          </View>
+          <View style = { styles.buttonContainer }>
+            <View style = { styles.button }>
+              <Text style = { this.state.fontLoaded ? styles.buttonText : styles.buttonTextElse }>Decline</Text>
+            </View>
+            <View style = { styles.button }>
+              <Text style = { this.state.fontLoaded ? styles.buttonText : styles.buttonTextElse }>Lets Go!</Text>
+            </View>
+          </View>
         </View>
       </View>
     )
   }
-
-  const styles = StyleSheet.create({
+}
+const styles = StyleSheet.create({
     bottomContainer: {
       backgroundColor: '#93E1FA',
+      alignItems: 'center',
+      flex: 1,
+      paddingTop: 25,
+    },
+    button: {
+      backgroundColor: '#FFC928',
+      justifyContent: 'center',
+      borderRadius: 20,
+      height: 75,
+      width: 125,
+      marginLeft: 20,
+      marginRight: 20,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      paddingTop: 30,
+    },
+    buttonText: {
+      textAlign: 'center',
+      fontFamily: 'source-sans-pro',
+      color: '#4F4F4F',
+      fontSize: 30,
+    },
+    buttonTextElse: {
+      textAlign: 'center',
+      color: '#4F4F4F',
+      fontSize: 30,
     },
     container: {
       flex: 2,
     },
     cup: {
-      width: 125,
+      width: 200,
       height: 125,
     },
-    match: {
+    details: {
+      borderRadius: 20,
+      backgroundColor: 'white',
+      width: '75%',
+      height: 175,
+      justifyContent: 'space-around',
+      paddingLeft: 15,
+    },
+    detailsText: {
+      fontSize: 30,
+      color: '#4F4F4F',
+      fontFamily: 'source-sans-pro',
+      textAlign: 'left',
+    },
+    detailsTextElse: {
+      fontSize: 30,
+      color: '#4F4F4F',
+      textAlign: 'left',
+    },
+    detailsHeader: {
+      backgroundColor: '#FFC928',
+      borderTopRightRadius: 20,
+      borderTopLeftRadius: 20,
+      width: '50%',
+      height: 40,
+      justifyContent: 'center',
+    },
+    detailsHeaderText: {
+      fontFamily: 'source-sans-pro',
+      color: '#4F4F4F',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      fontSize: 20,
+    },
+    detailsHeaderTextElse: {
+      color: '#4F4F4F',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      fontSize: 20,
+    },
+    matchText: {
       fontFamily: 'source-sans-pro',
       fontSize: 72,
-      fontAlign: 'center',
+      textAlign: 'center',
+      color: '#4F4F4F',
+      width: '60%',
     },
-    matchElse: {
+    matchTextElse: {
       fontSize: 72,
-      fontAlign: 'center',
+      textAlign: 'center',
+      color: '#4F4F4F',
     },
     topContainer: {
       backgroundColor: 'white',
       justifyContent: 'space-around',
+      alignItems: 'center',
+      flex: 1,
     },
     yellowStrip: {
       backgroundColor: '#FFC928',
-      height: '4%',
+      height: '1.5%',
     },
   });
