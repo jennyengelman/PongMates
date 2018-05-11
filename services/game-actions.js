fe2dd7220adb266dc448968cdf25357612de2378
Jenny Engelman <jenny.l.engelman.21@dartmouth.edu>
2018-05-10 23:11:28 -0400

    created branch

---
+++ services/game-actions.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase'
+import moment from 'moment';
+
+export function createGame(game) {
+  return new Promise((resolve, reject) => {
+    firebase.database().ref(`games/${game.id}`).set({
+      //creator: user.id,
+      id: game.id,
+      place: game.place,
+      year: game.year,
+      timestamp: moment.now(),
+    })
+    resolve(true)
+  })
+}
+
+export function generateGameKey() {
+  return new Promise((resolve, reject) => {
+    var key = firebase.database().ref().child('games').push().key
+    resolve(key)
+  })
+}
+
+export function getGame(game) {
+  return new Promise((resolve, reject) => {
+    firebase.database().ref(`games/${game.id}`).once('value').then(snapshot => {
+      resolve(snapshot.val())
+    });
+  })
+}
+
+export function deleteGame(game) {
+  return new Promise((resolve, reject) => {
+    firebase.database().ref(`games/${game.id}`).remove()
+    })
+    resolve(true)
+}
