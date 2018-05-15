7f588cd7e7bec531df60514ecf3ad61790841b24
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-15 11:43:45 -0400

    passes in entire user object rather than just id for game creation. able to find a game

--- services/database-actions.js
+++ services/database-actions.js
@@ -1,24 +1,37 @@
 import firebase from 'firebase'
-import { getUser } from './../services/user-actions.js'
 import { updateGame } from './../services/game-actions'
 
 
 export function searchDatabase(preferences, user) {
   return new Promise((resolve, reject) => {
+    let childID = undefined
+    let userID = undefined
     firebase.database().ref("games").once("value").then((snapshot) => {
-      snapshot.forEach(function(childSnapshot) {
-        if (childSnapshot.val().year.includes(user.year)) {
+
+      for (let key in snapshot.val()) {
+        const childSnapshot = snapshot.val()[key]
+        console.log(childSnapshot)
+        console.log(user)
+        console.log(preferences)
+        if (childSnapshot.year.some(item => user.year)) {
           //finder is right age
-          if (preferences.place.includes(childSnapshot.val().place)) {
+          if (preferences.place.some(item => childSnapshot.place.includes(item))) {
             //creator has right place
-            if (preferences.year.includes(childSnapshot.val().creatoryear)) {
+            if (preferences.year.includes(childSnapshot.creator.year)) {
               //creator is right age
-              resolve(updateGame(childSnapshot.val().id, user.id))
+              if (!childID && !userID) {
+                childID = childSnapshot.id
+                userID = user.id
+              }
             }
           }
         }
-      })
-      reject(false)
+      }
+      if (childID && userID) {
+        resolve(updateGame(childID, userID))
+      } else {
+        reject(false)
+      }
     })
   })
 }
